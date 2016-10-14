47e5de2417df1e960407fa2e68e692ad64b7dfb5
toby <toby.xu@tpv-tech.com>
2016-10-14 17:02:19 +0800

    modify

--- src/com/xum/study/LockActivity.java
+++ src/com/xum/study/LockActivity.java
@@ -1,13 +1,20 @@
 package com.xum.study;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
 
 public class LockActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_lock);
+		View lock_ui = getLayoutInflater().inflate(R.layout.activity_lock, null);
+		//setContentView(lock_ui);
+		new AlertDialog.Builder(this).setTitle("请输入")  
+        .setIcon(android.R.drawable.ic_dialog_info)  
+        .setView(lock_ui).setPositiveButton("确定", null)  
+        .setNegativeButton("取消", null).show(); 
 	}
 
 }
--- src/com/xum/study/MainActivity.java
+++ src/com/xum/study/MainActivity.java
@@ -1,19 +1,52 @@
 package com.xum.study;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     
 	public final static String LockActivity_Intent = "com.xum.intent.action.LockActivity";
+	public final static String TAG = "MainActivity";
+	public final static int LOCK_REQUEST_TO_LOCK = 0;
+	private TextView resultText;
+	private Button show;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		show.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(LockActivity_Intent);
+				startActivityForResult(i, MainActivity.LOCK_REQUEST_TO_LOCK);
+			}
+		});
 	}
 
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d(TAG,"onActivityResult " + requestCode + " " + resultCode);
+		if(requestCode == MainActivity.LOCK_REQUEST_TO_LOCK){
+			switch(resultCode){
+			    case 1: //Matched
+			    	Log.d(TAG,"success match");
+			    	// do something ...
+			    	resultText.setText("success!");
+			    	break;
+			}
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
